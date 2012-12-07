12d64871f7956cbcb60136b4c10a9fc344c48559
Dzmitry Zaitsau <Dzmitry_Zaitsau@epam.com>
2012-12-07 21:16:27 +0300

    fromString conversion for Nucleotide

--- Lib/Nucleotide.hs
+++ Lib/Nucleotide.hs
@@ -1,3 +1,9 @@
-module Lib.Nucleotide where
+module Lib.Nucleotide 
+	(stringToNucleotides, Nucleotide(..))
+	where
 
 data Nucleotide = A | G | C | U | T deriving (Eq, Ord, Show, Read )
+
+stringToNucleotides :: String -> [Nucleotide]
+stringToNucleotides = map charToNucleotide
+	where charToNucleotide ch = read [ch]
