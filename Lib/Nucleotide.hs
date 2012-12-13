cf655115190e384cd0b2ef2ae270ce236b3e567c
Pradd <pradd.me@gmail.com>
2012-12-14 02:06:52 +0300

    toString conversion for [Nucleotide]

--- Lib/Nucleotide.hs
+++ Lib/Nucleotide.hs
@@ -1,5 +1,5 @@
 module Lib.Nucleotide 
-	(stringToNucleotides, Nucleotide(..))
+	(stringToNucleotides, nucleotidesToString, Nucleotide(..))
 	where
 
 data Nucleotide = A | G | C | U | T deriving (Eq, Ord, Show, Read )
@@ -7,3 +7,6 @@ data Nucleotide = A | G | C | U | T deriving (Eq, Ord, Show, Read )
 stringToNucleotides :: String -> [Nucleotide]
 stringToNucleotides = map charToNucleotide
 	where charToNucleotide ch = read [ch]
+
+nucleotidesToString :: [Nucleotide] -> String
+nucleotidesToString = concatMap show
